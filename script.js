fd0ac0fdb3233968d64f265ced6c2ede04df50dc
OjoMayowa29 <161862754+OjoMayowa29@users.noreply.github.com>
2024-04-07 18:53:24 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,80 @@
+//Medium
+function squared(b) {
+	return  b * b
+}
+
+//Easy
+function hello() {
+	 return "hello edabit.com"
+}
+
+//Easy
+function addition(a, b) {
+	return a + b
+}
+
+//Easy
+function convert(minutes) {
+	return minutes *60
+}
+
+//Easy
+function addition(num) {
+	return num +1
+}
+
+//easy
+function triArea(base, height) {
+	return (base * height) / 2
+}
+
+//Easy
+function calcAge(age) {
+	return age *365
+}
+
+//Easy
+function cubes(a) {
+	return a ** 3
+}
+
+//Easy
+function getFirstValue(arr) {
+	return arr [0]
+}
+
+//Easy
+function circuitPower(voltage, current) {
+	return voltage * current
+}
+
+//Easy
+function findPerimeter(length, width) {
+	return (length + width) * 2
+}
+
+//Easy
+function sumPolygon(n) {
+	return (n - 2) * 180
+}
+
+//Medium
+function howManySeconds(hours) {
+	return hours * 60 * 60
+}
+
+//Easy
+function nextEdge(side1, side2) {
+	return (side1 + side2) - 1
+}
+
+//Medium
+function printArray(number) {
+  var newArray = [];
+
+  for(var i = 1; i <= number; i++) {
+    newArray.push(i);
+  }
+
+  return newArray;
+}
